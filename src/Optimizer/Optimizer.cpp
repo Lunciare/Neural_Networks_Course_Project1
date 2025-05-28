0645f7041fe5ddceb6f7eccd0a4f0e1b8ee29fd2
Lunciare <145194562+Lunciare@users.noreply.github.com>
2025-05-28 14:40:31 +0300

    Change Optimizer

--- src/Optimizer/Optimizer.cpp
+++ src/Optimizer/Optimizer.cpp
@@ -3,34 +3,49 @@
 
 namespace neural_network {
 
-Optimizer::Optimizer(double lr, double beta1, double beta2, double eps,
-                     OptimizerType type)
-    : lr_(lr), beta1_(beta1), beta2_(beta2), eps_(eps), type_(type) {}
+Optimizer::Optimizer(
+    std::function<void(Matrix &, Matrix &, Matrix &, const Matrix &, Index)> um,
+    std::function<void(Vector &, Vector &, Vector &, const Vector &, Index)> uv)
+    : update_matrix_(std::move(um)), update_vector_(std::move(uv)) {}
 
-void Optimizer::update(Matrix &param, Matrix &m, Matrix &v, const Matrix &grad,
-                       Index t) const {
-  if (type_ == OptimizerType::SGD) {
-    param -= lr_ * grad;
-  } else if (type_ == OptimizerType::Adam) {
-    m = beta1_ * m + (1.0 - beta1_) * grad;
-    v = beta2_ * v + (1.0 - beta2_) * grad.array().square().matrix();
-    Matrix m_hat = m / (1.0 - std::pow(beta1_, t));
-    Matrix v_hat = v / (1.0 - std::pow(beta2_, t));
-    param -= lr_ * (m_hat.array() / (v_hat.array().sqrt() + eps_)).matrix();
-  }
+Optimizer Optimizer::SGD(double lr) {
+  auto update_m = [lr](Matrix &p, Matrix &, Matrix &, const Matrix &g, Index) {
+    p -= lr * g;
+  };
+  auto update_v = [lr](Vector &p, Vector &, Vector &, const Vector &g, Index) {
+    p -= lr * g;
+  };
+  return Optimizer(update_m, update_v);
 }
 
-void Optimizer::update(Vector &param, Vector &m, Vector &v, const Vector &grad,
+Optimizer Optimizer::Adam(double lr, double beta1, double beta2, double eps) {
+  auto update_m = [=](Matrix &p, Matrix &m, Matrix &v, const Matrix &g,
+                      Index t) {
+    m = beta1 * m + (1.0 - beta1) * g;
+    v = beta2 * v + (1.0 - beta2) * g.array().square().matrix();
+    Matrix m_hat = m / (1.0 - std::pow(beta1, t));
+    Matrix v_hat = v / (1.0 - std::pow(beta2, t));
+    p -= lr * (m_hat.array() / (v_hat.array().sqrt() + eps)).matrix();
+  };
+  auto update_v = [=](Vector &p, Vector &m, Vector &v, const Vector &g,
+                      Index t) {
+    m = beta1 * m + (1.0 - beta1) * g;
+    v = beta2 * v + (1.0 - beta2) * g.array().square().matrix();
+    Vector m_hat = m / (1.0 - std::pow(beta1, t));
+    Vector v_hat = v / (1.0 - std::pow(beta2, t));
+    p -= lr * (m_hat.array() / (v_hat.array().sqrt() + eps)).matrix();
+  };
+  return Optimizer(update_m, update_v);
+}
+
+void Optimizer::update(Matrix &p, Matrix &m, Matrix &v, const Matrix &g,
                        Index t) const {
-  if (type_ == OptimizerType::SGD) {
-    param -= lr_ * grad;
-  } else if (type_ == OptimizerType::Adam) {
-    m = beta1_ * m + (1.0 - beta1_) * grad;
-    v = beta2_ * v + (1.0 - beta2_) * grad.array().square().matrix();
-    Vector m_hat = m / (1.0 - std::pow(beta1_, t));
-    Vector v_hat = v / (1.0 - std::pow(beta2_, t));
-    param -= lr_ * (m_hat.array() / (v_hat.array().sqrt() + eps_)).matrix();
-  }
+  update_matrix_(p, m, v, g, t);
+}
+
+void Optimizer::update(Vector &p, Vector &m, Vector &v, const Vector &g,
+                       Index t) const {
+  update_vector_(p, m, v, g, t);
 }
 
 } // namespace neural_network
--- src/Optimizer/Optimizer.h
+++ src/Optimizer/Optimizer.h
@@ -1,36 +1,33 @@
 #pragma once
 
 #include "Utilities/LinAlg.h"
+#include <functional>
+#include <memory>
 
 namespace neural_network {
 
-// Supported optimizer types
-enum class OptimizerType { SGD, Adam };
-
-// Optimizer class: applies SGD or Adam update rule
 class Optimizer {
 public:
-  // Constructor: default is Adam
-  Optimizer(double lr, double beta1 = 0.9, double beta2 = 0.999,
-            double eps = 1e-8, OptimizerType type = OptimizerType::Adam);
+  static Optimizer SGD(double lr);
+  static Optimizer Adam(double lr, double beta1 = 0.9, double beta2 = 0.999,
+                        double eps = 1e-8);
 
-  // Update parameter matrix in place
   void update(Matrix &param, Matrix &m, Matrix &v, const Matrix &grad,
               Index t) const;
-
-  // Update parameter vector in place
   void update(Vector &param, Vector &m, Vector &v, const Vector &grad,
               Index t) const;
 
-  // Get optimizer type
-  OptimizerType type() const { return type_; }
-
 private:
-  double lr_;    // Learning rate
-  double beta1_; // Exponential decay for first moment
-  double beta2_; // Exponential decay for second moment
-  double eps_;   // Small value for numerical stability
-  OptimizerType type_;
+  std::function<void(Matrix &, Matrix &, Matrix &, const Matrix &, Index)>
+      update_matrix_;
+  std::function<void(Vector &, Vector &, Vector &, const Vector &, Index)>
+      update_vector_;
+
+  Optimizer(
+      std::function<void(Matrix &, Matrix &, Matrix &, const Matrix &, Index)>
+          um,
+      std::function<void(Vector &, Vector &, Vector &, const Vector &, Index)>
+          uv);
 };
 
 } // namespace neural_network
